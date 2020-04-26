d101c817439b9fb6e42efbafc6c582d9d059a145
Sydney Vespa <61509237+S-Vespa@users.noreply.github.com>
2020-04-26 19:57:20 -0400

    first commit

--- ArtMuseum.Data/Museums.cs
+++ ArtMuseum.Data/Museums.cs
@@ -24,6 +24,6 @@ namespace ArtMuseum.Data
         [Required]
         public int CountryCode { get; set; }
 
-        public List<string> CollectedWorks = new List<string>();
+        public ICollection<string> CollectedWorks { get; set; }
     }
 }
