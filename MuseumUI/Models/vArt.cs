375f96d36aa3a2ffb62b421e167450f1c7a32f48
M1K00 <61482244+M1K00@users.noreply.github.com>
2020-05-01 14:36:38 -0400

    started ui

---
+++ MuseumUI/Models/vArt.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MuseumUI.Models
+{
+    public enum Types { FrescoPainting = 1, Painting, Sculpture }
+
+    public enum Era { Renaissance = 1, Realism, Hellenistic, Impressionism, Romantic, Baroque }
+
+    public class vArt
+    {
+        public int ArtworkId { get; set; }
+        public string NameOfPiece { get; set; }
+        public string Artist { get; set; }
+        public string Description { get; set; }
+        public int MuseumId { get; set; }
+        public bool Availability { get; set; }
+        public string Medium { get; set; }
+        public Types Types { get; set; }
+        public Era Era { get; set; }
+
+        [ForeignKey(nameof(MuseumId))]
+        public virtual Museum Museum { get; set; }
+    }
+}
---
+++ MuseumUI/MuseumApiService.cs
@@ -0,0 +1,39 @@
+﻿using MuseumUI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http.Formatting;
+
+
+namespace MuseumUI
+{
+    public class MuseumApiService
+    {
+        private readonly HttpClient _httpClient = new HttpClient();
+
+        public async Task<vArt> GetArtworkAsync(string url)
+        {
+            HttpResponseMessage response = await _httpClient.GetAsync(url);
+
+            if (response.IsSuccessStatusCode)
+            {
+                vArt art = await response.Content.ReadAsAsync<vArt>();
+                return art;
+            }
+            return null;
+        }
+
+        public async Task<T> GetAsync<T>(string url)
+        {
+            HttpResponseMessage response = await _httpClient.GetAsync(url);
+            if (response.IsSuccessStatusCode)
+            {
+                return await response.Content.ReadAsAsync<T>();
+            }
+            return default;
+        }
+    }
+}
